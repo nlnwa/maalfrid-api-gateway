4ada4b2448672f541f72c4dd0854b60bf9b882c7
Marius Elsfjordstrand Beck <marius.beck@nb.no>
2017-08-23 14:40:13 +0200

    Change API from path parameter to query parameter

--- controllers/api.js
+++ controllers/api.js
@@ -1,44 +1,50 @@
 const maalfrid = require('../service/maalfrid');
 const config = require('../config');
 const r = require('rethinkdbdash')(config.rethinkdb);
+const url = require('url');
 
-function getFromDatabase(params) {
-  const uri = params.uri;
-  //  const lix = params.lix;
-  //  const wc = params.wc;
+function getFromDatabase(query) {
+  const uri = query.url;
+  const hostname = url.parse(uri).hostname; 
+  const lix = parseInt(query.lix) || 0;
+  const wc = parseInt(query.wc) || 0;
+
+  console.log(`uri ${uri} :: lix ${lix} :: wc ${wc}`);
 
   return r.table('crawl_log')
     .filter((crawl) =>
-            crawl('requestedUri').eq(uri)
-            .or(crawl('referrer').match(`^${uri}`))
+            crawl('requestedUri').match(`^${uri}`)
+            .or(crawl('referrer').match(`^${hostname}`))
            )
     .eqJoin('warcId', r.table('extracted_text'))
     .getField('right')
-    .filter(r.row('characterCount').gt(100))
+    .filter(r.row('wordCount').gt(wc).and(r.row('lix').gt(lix)))
     .getField('text')
     .run();
 }
 
-exports.stats = (req, res) =>
-  getFromDatabase(req.params)
-  .then((data) =>
-        Promise.all(data.map((text) => maalfrid.detectLanguage(text)))
-        .then((codes) => {
-          let total = codes.length;
-          let unique = {};
+exports.stats = (req, res) => {
+  getFromDatabase(req.query)
+    .then((data) =>
+          Promise.all(data.map((text) => maalfrid.detectLanguage(text)))
+          .then((codes) => {
+            let total = codes.length;
+            let unique = {};
 
-          codes.map((value) => {
-            if (!(value in unique)) {
-              unique[value] = 1;
-            } else {
-              unique[value] += 1;
+            codes.map((value) => {
+              if (!(value in unique)) {
+                unique[value] = 1;
+              } else {
+                unique[value] += 1;
+              }
+            });
+
+            let result = [];
+            for (const key of Object.keys(unique)) {
+              let entry = {};
+              entry[key] = unique[key] / total;
+              result.push( entry );
             }
-          });
-          let result = [];
-          for (const key of Object.keys(unique)) {
-            let entry = {};
-            entry[key] = unique[key] / total;
-            result.push( entry );
-          }
-          res.json(result);
-        }).catch((err) => console.error(err)));
+            res.json(result);
+          }).catch((err) => console.error("ERROR" + err)));
+};
--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const api = require('../controllers/api');
 
-router.get('/stats/:uri/:lix?/:wc?', api.stats);
+router.get('/stats', api.stats);
 
 router.get('/',
            (req, res) => res.status(200).send('/api/stats/:uri/:lix?/:wc?'));
