cb72876fd13e409690c2a00f59c970ba8949af80
Marius Elsfjordstrand Beck <marius.beck@nb.no>
2017-08-29 13:37:46 +0200

    Revert "Round robin service requests to the cluster"
    This reverts commit ba0d3567fbd5d607adc89679861480d048194048.

--- service/maalfrid.js
+++ service/maalfrid.js
@@ -1,30 +1,17 @@
 const config = require('../config');
 const grpc = require('grpc');
 const api = grpc.load(config.sproett.proto_path).maalfrid;
+const address = config.sproett.host + ':' + config.sproett.port;
 
-const poolSize = require('os').cpus().length;
-const pool = [];
-for (let i = 0; i < poolSize; i++) {
-  const address = config.sproett.host + ':' + (config.sproett.port + i);
-  pool.push(new api.Maalfrid(address, grpc.credentials.createInsecure()));
-}
-let current = 0;
-function getService() {
-  const service = pool[current];
-  current += 1;
-  if (current == poolSize) {
-    current = 0;
-  }
-  return service;
-}
+const maalfrid = new api.Maalfrid(address, grpc.credentials.createInsecure());
 
 exports.detectLanguage = (text) => {
-  return new Promise((resolve, reject) => {
-      getService().detectLanguage({text: text}, (err, response) => {
+    return new Promise((resolve, reject) => {
+        maalfrid.detectLanguage({text: text}, (err, response) => {
             if (err) {
                 return reject(err);
             }
             return resolve(response.language);
-      });
+        });
     });
 };
